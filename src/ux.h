4fcce98bb333613a6d3d8f9125cc147ebcec7b4a
Yan Martins <yan.martins@gmail.com>
2019-11-05 16:36:22 +0000

    chore: add license

--- src/ux.h
+++ src/ux.h
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // ux is a magic global variable implicitly referenced by the UX_ macros. Apps
 // should never need to reference it directly.
 extern ux_state_t ux;
